cdd885698647527d768adab250d1e2aef6cb9437
Omar <omar.khatib@gmail.com>
2021-04-15 16:26:19 -0400

    Fixed phase calculation

--- utils/optical_constants.py
+++ utils/optical_constants.py
@@ -64,22 +64,27 @@ def matrix_method_slab(er, mr, d, f):
     z = real_check.apply(z)
 
     # R2 = sq(abs(div((mr - n), (mr + n))))
-    # PhiR2 = arctan(div(2 * n.imag, (1 - sq(n.imag) - sq(n.imag))))
+    r = abs(div((mr - n), (mr + n)))
+    phiR2 = arctan(div(2 * n.imag, (1 - sq(n.real) - sq(n.imag))))
     # T2 = exp(-2 * mul(k.imag, d)) * sq((div(n, mr)).real * sq(abs(div(2 * mr, (n + mr)))))
     # T3 = mul(exp(-2 * mul(k.imag, d)), sq(
     #     (div(mul(n.real, mu.real)
     #          + mul(n.imag, mu.imag), (sq(mu.real) +
     #                                   sq(mu.imag)))) * m0 * sq(abs(div(2 * mr, (n + mr))))))
-    # PhiT2 = arctan(div(-2 * n.imag, (1 - sq(n.imag) - sq(n.imag))))
+    t = exp(-2 * mul(k.imag, d)) * (div(n, mr).real * abs(div(2 * mr, (n + mr))))
+    lt = div(2*mr,(n+mr))
+    phiT2 = arctan(div(lt.imag, lt.real))
 
+    r = mul(r, exp(1j * phiR2))
+    t = mul(t, exp(1j * phiT2))
     # M12_TE = cos(mul(k, d)) + 0.5*1j*mul((mul(div(1, mr + 1e-5), div(k, k0)) + mul(mr, div(k0, k))), (sin(mul(k, d))))
     # M22_TE = cos(mul(k, d)) - 0.5*1j*mul((mul(div(1, mr + 1e-5), div(k, k0)) + mul(mr, div(k0, k))), (sin(mul(k, d))))
 
-    M12_TE = 0.5 * 1j * mul((z - div(1, z)), (sin(mul(k, d))))
-    M22_TE = cos(mul(k, d)) - 0.5 * 1j * mul((z + div(1, z)), (sin(mul(k, d))))
-
-    r = div(M12_TE,M22_TE + 1e-5)
-    t = div(1, M22_TE + 1e-5)
+    # M12_TE = 0.5 * 1j * mul((z - div(1, z + 1e-5)), (sin(mul(k, d))))
+    # M22_TE = cos(mul(k, d)) - 0.5 * 1j * mul((z + div(1, z + 1e-5)), (sin(mul(k, d))))
+    #
+    # r = div(M12_TE,M22_TE + 1e-5)
+    # t = div(1, M22_TE + 1e-5)
 
     # T = (mul(t, conj(t)).real).float()
     # R = (mul(r, conj(r)).real).float()
@@ -89,8 +94,8 @@ def lorentzian(w, w0, wp , g, eps_inf=0):
     num1 = mul(sq(wp), add(sq(w0), -sq(w)))
     num2 = mul(sq(wp), mul(w, g))
     denom = add(sq(add(sq(w0), -sq(w))), mul(sq(w), sq(g)))
-    e1 = div(num1, denom)
-    e2 = div(num2, denom)
+    e1 = div(num1, denom + 1e-5)
+    e2 = div(num2, denom + 1e-5)
     e1 += eps_inf
     return e1,e2
 
@@ -145,13 +150,13 @@ class matrix_method_slab_debug:
         # R = (mul(r, conj(r)).real).float()
 
         R2 = sq(abs(div((mr - n),(mr + n))))
-        PhiR2 = arctan(div(2*n.imag,(1-sq(n.imag)-sq(n.imag))))
+        phiR2 = arctan(div(2*n.imag,(1-sq(n.real)-sq(n.imag))))
         T2 = exp(-2*mul(k.imag,d))*sq((div(n,mr)).real*sq(abs(div(2*mr,(n+mr)))))
         T3 = mul(exp(-2*mul(k.imag,d)), sq(
                     (div(mul(n.real,mu.real)
                          + mul(n.imag,mu.imag),(sq(mu.real) +
                                                 sq(mu.imag)))) * m0 * sq(abs(div(2*mr,(n + mr))))))
-        PhiT2 = arctan(div(-2 * n.imag, (1 - sq(n.imag) - sq(n.imag))))
+        phiT2 = arctan(div(-2 * n.imag, (1 - sq(n.real) - sq(n.imag))))
 
         self.w = f
         self.k0 = k0
@@ -170,10 +175,10 @@ class matrix_method_slab_debug:
         self.T = sq(abs(t))
         self.R = sq(abs(r))
         self.R2 = R2
-        self.PhiR2 = PhiR2
+        self.phiR2 = phiR2
         self.T2 = T2
         self.T3 = T3
-        self.PhiT2 = PhiT2
+        self.phiT2 = phiT2
         self.theta = theta
         self.magic = magic
 
