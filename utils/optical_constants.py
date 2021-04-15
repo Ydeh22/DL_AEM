4556a358806844994064bd27e677be7d703cb9ea
Omar <omar.khatib@gmail.com>
2021-04-15 16:54:45 -0400

    Debug optical constants

--- utils/optical_constants.py
+++ utils/optical_constants.py
@@ -64,19 +64,17 @@ def matrix_method_slab(er, mr, d, f):
     z = real_check.apply(z)
 
     # R2 = sq(abs(div((mr - n), (mr + n))))
-    r = abs(div((mr - n), (mr + n)))
-    phiR2 = arctan(div(2 * n.imag, (1 - sq(n.real) - sq(n.imag))))
+    r = div((mr - n), (mr + n))
+    # phiR2 = arctan(div(2 * n.imag, (1 - sq(n.real) - sq(n.imag))))
     # T2 = exp(-2 * mul(k.imag, d)) * sq((div(n, mr)).real * sq(abs(div(2 * mr, (n + mr)))))
     # T3 = mul(exp(-2 * mul(k.imag, d)), sq(
     #     (div(mul(n.real, mu.real)
     #          + mul(n.imag, mu.imag), (sq(mu.real) +
     #                                   sq(mu.imag)))) * m0 * sq(abs(div(2 * mr, (n + mr))))))
-    t = exp(-2 * mul(k.imag, d)) * (div(n, mr).real * abs(div(2 * mr, (n + mr))))
-    lt = div(2*mr,(n+mr))
-    phiT2 = arctan(div(lt.imag, lt.real))
+    # t = exp(-2 * mul(k.imag, d)) * (div(n, mr).real * abs(div(2 * mr, (n + mr))))
+    t = exp(-2 * mul(k.imag, d)) * div(2*mr,(n+mr))
+    # phiT2 = arctan(div(lt.imag, lt.real))
 
-    r = mul(r, exp(1j * phiR2))
-    t = mul(t, exp(1j * phiT2))
     # M12_TE = cos(mul(k, d)) + 0.5*1j*mul((mul(div(1, mr + 1e-5), div(k, k0)) + mul(mr, div(k0, k))), (sin(mul(k, d))))
     # M22_TE = cos(mul(k, d)) - 0.5*1j*mul((mul(div(1, mr + 1e-5), div(k, k0)) + mul(mr, div(k0, k))), (sin(mul(k, d))))
 
@@ -149,14 +147,15 @@ class matrix_method_slab_debug:
         # T = (mul(t, conj(t)).real).float()
         # R = (mul(r, conj(r)).real).float()
 
-        R2 = sq(abs(div((mr - n),(mr + n))))
-        phiR2 = arctan(div(2*n.imag,(1-sq(n.real)-sq(n.imag))))
-        T2 = exp(-2*mul(k.imag,d))*sq((div(n,mr)).real*sq(abs(div(2*mr,(n+mr)))))
-        T3 = mul(exp(-2*mul(k.imag,d)), sq(
-                    (div(mul(n.real,mu.real)
-                         + mul(n.imag,mu.imag),(sq(mu.real) +
-                                                sq(mu.imag)))) * m0 * sq(abs(div(2*mr,(n + mr))))))
-        phiT2 = arctan(div(-2 * n.imag, (1 - sq(n.real) - sq(n.imag))))
+        R2 = sq(abs(div((mr - n), (mr + n))))
+        phiR2 = arctan(div(2 * n.imag, (1 - sq(n.real) - sq(n.imag))))
+        T2 = exp(-2 * mul(k.imag, d)) * sq((div(n, mr)).real * sq(abs(div(2 * mr, (n + mr)))))
+        T3 = mul(exp(-2 * mul(k.imag, d)), sq(
+            (div(mul(n.real, mu.real)
+                 + mul(n.imag, mu.imag), (sq(mu.real) +
+                                          sq(mu.imag)))) * m0 * sq(abs(div(2 * mr, (n + mr))))))
+        lt = div(2 * mr, (n + mr))
+        phiT2 = arctan(div(lt.imag, lt.real))
 
         self.w = f
         self.k0 = k0
